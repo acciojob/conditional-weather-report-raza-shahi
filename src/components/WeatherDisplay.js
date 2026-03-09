32e8d1c7b5f2b84317212b86c1e9d206b2da2a2f
Shahid Raza <raza59261@gmail.com>
2026-03-09 12:03:34 +0530

    conditions fixed

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -5,7 +5,7 @@ const WeatherDisplay = ({ weather }) => {
     <div>
       <h2>Weather Display</h2>
       <p><span style={weather.temperature>20 ? {color: "red"} : {color: "blue"}}>Temperature: {weather.temperature}</span></p>
-      <p>Condition: {weather.condition}</p>
+      <p>Conditions: {weather.conditions}</p>
     </div>
   )
 }
