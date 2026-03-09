e39e4c79f18f87ca254ec9bf22b64881496c5f61
Shahid Raza <raza59261@gmail.com>
2026-03-09 11:22:55 +0530

    span fix

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -4,7 +4,7 @@ const WeatherDisplay = ({ weather }) => {
   return (
     <div>
       <h2>Weather Display</h2>
-      <p>Temperature: <span style={weather.temperature>20 ? {color: "red"} : {color: "blue"}}>{weather.temperature}</span>°C</p>
+      <p><span style={weather.temperature>20 ? {color: "red"} : {color: "blue"}}>Temperature: {weather.temperature}°C</span></p>
       <p>Condition: {weather.condition}</p>
     </div>
   )
