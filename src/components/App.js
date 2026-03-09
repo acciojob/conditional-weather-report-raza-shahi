454594ec4f3d834eac733198b57f346de68e5bbb
Shahid Raza <raza59261@gmail.com>
2026-03-09 11:37:27 +0530

    fixed temp value

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
   const [weather, setWeather] = useState({
-    temperature: 10,
+    temperature: 25,
     condition: "Sunny",
   });
   return (
